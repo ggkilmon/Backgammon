902ffacd4e881e1d371b19d9548b5592076300b8
Graham Kilmon <graham_kilmon@hermanmiller.com>
2019-12-07 19:57:36 -0500

    initial create

---
+++ Backgammon/Backgammon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Backgammon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
